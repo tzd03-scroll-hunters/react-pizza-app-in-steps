f8fd5983127d255ae805267ce20ff62517b3eda0
losrobbos <losrobbos@web.de>
2025-06-27 09:37:09 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      {/* Component component 1: Header */}
+      <h1>Pizza Store</h1>
+      
+      <nav style={{ display: "flex", gap: 10, justifyContent: "center" }}>
+        <a href="">Pizzas</a>
+        <a href="">Cart</a>
+      </nav>
+    </>
+  )
+}
+
+export default App
