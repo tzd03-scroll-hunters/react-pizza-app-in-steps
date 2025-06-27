a4602c9c2d547a089a6a5102fb337940744cdaeb
losrobbos <losrobbos@web.de>
2025-06-27 10:02:09 +0200

    Created by laout wireframe with header, nav,  pizza list container

---
+++ PLAN.md
@@ -0,0 +1,37 @@
+# Plan
+
+## Mockup / Wireframe
+
+Erstelle Wireframe
+Über Wireframe bekommen wir COMPONENTS
+
+Alles was sich WIEDERHOLT, ist definitiv immer eine Component.
+
+Am Ende haben wir eine erste Liste an Componentns.
+
+Beispiel Pizza App:
+https://pizza-store-two.vercel.app/
+
+Erste Component Liste:
+- Header
+- Nav
+- Search Box
+- PizzaList
+- PizzaCard
+
+Für jede Component, die IMMER sichtbar ist und sich nicht ändert (Beispiel Header) kann man auch das HTML direkt in die App.jsx packen.
+
+## VsCode
+
+Für jede Component ein File anlegen (ein .jsx File)
+
+## CSS Reset
+
+Alle Default Styles von React raus
+
+## Dummy Content für Components
+
+Einfach erstmal Dummy Content ohne State und Logik.
+
+Dann kann ich erstmal, ohne großen Stress, die Components anordnen (Flex, Grid).
+
--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import './App.css'
 
 function App() {
 
+  // STARTEN wir nur mit DUMMY Content
+  // nur HTML / Layout CSS / keine Detail Stylings
+  // keine Logik (keine Click Listener, kein State, etc)
   return (
     <>
       {/* Component 1: Header */}
@@ -12,6 +15,26 @@ function App() {
         <a href="">Pizzas</a>
         <a href="">Cart</a>
       </nav>
+
+      {/* Component 3: Search Box */}
+      <input type="text" placeholder='Search term...' />
+
+      {/* Component 4: Pizza List Container */}
+      <main>
+
+        {/* Component 5: Pizza Card */}
+        <div className="pizza-list" style={{ 
+          display: "flex", 
+          gap: "1em",
+          padding: "2em",
+          flexDirection: "column"
+          }}>
+          <div className="card">Pizza 1</div>
+          <div className="card">Pizza 2</div>
+          <div className="card">Pizza 3</div>
+        </div>
+
+      </main>
     </>
   )
 }
