4bff636d827b19005bf5b63f0b80e5fad7ace1b1
losrobbos <losrobbos@web.de>
2025-06-27 09:37:42 +0200

    Added first two components

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,10 @@ function App() {
 
   return (
     <>
-      {/* Component component 1: Header */}
+      {/* Component 1: Header */}
       <h1>Pizza Store</h1>
-      
+
+      {/* Component 2: Nav */}
       <nav style={{ display: "flex", gap: 10, justifyContent: "center" }}>
         <a href="">Pizzas</a>
         <a href="">Cart</a>
